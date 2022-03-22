22a8cf6acd2c95ba5d71fc1dc8d14aa939df2055
RoshimaRamesh <101967559+RoshimaRamesh@users.noreply.github.com>
2022-03-22 11:39:18 +0530

    Add files via upload

---
+++ NegToPosNumber.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class NegToPosNumber {
+
+	public static void main(String[] args) {
+		
+		
+		int number = -40;
+		int no;
+		
+		if(number < 0);
+		{
+			no = -number;
+			System.out.println("number is positive:"+no);
+			
+		}
+
+	}
+
+}
---
+++ NumberPosorNeg.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class NumberPosorNeg {
+
+	public static void main(String[] args) {
+		
+		int number = 35;
+		
+		if(number>0)
+		{
+			System.out.println("the number is positive:"+number);
+		}
+		else
+		{
+			System.out.println("the number is negative:"+number);
+		}
+		
+	}
+
+}
